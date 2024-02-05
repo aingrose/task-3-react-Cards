a1a9827c769493adeb8e1278d32ddf3a5203fe43
MAHESHKUMAR A <120610732+aingrose@users.noreply.github.com>
2024-02-05 12:24:27 +0530

    Update Box.js

--- src/Box.js
+++ src/Box.js
@@ -1,13 +1,20 @@
-import React from 'react'
+
+import React from 'react';
 
 function Box() {
   return (
-    <div className='container-fluid bg-dark text-white 'style={{padding:'90px'}}>
-    <h1 style={{fontSize: '60px',fontWeight: "bold"}}>Shop in style</h1>
-    <p className="text-white-50"style={{fontSize: '20px'}}>With this shop homepeage template</p>
-        
+    <div
+      className='container-fluid text-white'
+      style={{
+        padding: '90px',
+        backgroundColor: '#FFC074',
+        backgroundImage: 'radial-gradient(at 47% 33%, hsl(12.00, 90%, 40%) 0, transparent 59%), radial-gradient(at 82% 65%, hsl(198.00, 100%, 50%) 0, transparent 55%)',
+      }}
+    >
+      <h1 className='box-textt' style={{ fontSize: '60px', fontWeight: 'bold' }}>Exploring Global Cuisines</h1>
+      <p className="text-white-50" style={{ fontSize: '20px' }}>Perfect Pairings for Food and Drinks</p>
     </div>
-  )
+  );
 }
 
-export default Box
+export default Box;
